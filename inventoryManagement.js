9ce11253f425d1483d799a079460f674f2d52c3a
Danish <dkhans2001@gmail.com>
2025-05-10 20:45:55 -0500

    inventory

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,30 @@
 // Write your code here
 
+// Create the products array
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+// Log the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+// Add a new product to the array
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// Update a product name at a given position
+function updateProductName(position, newName) {
+  if (position >= 0 && position < products.length) {
+    products[position] = newName;
+  }
+}
+
+// Remove the last product in the array
+function removeLastProduct() {
+  products.pop();
+}
+
 
 
 // Export the necessary parts for testing
